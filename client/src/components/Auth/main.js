93fde1f16559acc1d37081a3881e2cdc7df8d8a7
rzanabusamra22 <rzanabusamra@gmail.com>
2021-01-26 13:15:05 +0200

    Main css

--- client/src/components/Auth/main.js
+++ client/src/components/Auth/main.js
@@ -14,24 +14,25 @@ function Main() {
 
     return (
         <>
-          <div class="container" class="main">
+          <div class="container" class="main" >
              <div class="row">
-                <div class="col">
-                    1 of 2
+                <div class="col"  >
+                    <img src="https://i.imgur.com/Pfnb4uP.png"  style={{objectFit: "cover",width:"779px", height:"100%", marginTop:"5px"}}/>
                </div>
-               <div class="col" id="right-side">
-                    <div className="auth-page__logo" >
+
+               <div class="col" id="right-side" >
+                    <div className="auth-page__logo" style={{ marginRight: "390px"}}>
                         <Button icon><Twitter /></Button>
                     </div>
-                    <TextTitle title style={{ fontSize: "30px", marginBottom: "70px" , marginTop: "10px"}}><b>See what’s happening in <br></br> the world right now</b></TextTitle>
-                    <TextTitle title style={{ fontSize: "16px", marginBottom: "15px" }}><b>Join Twitter today.</b></TextTitle>
+                    <TextTitle title style={{ fontSize: "30px", marginBottom: "70px" , marginTop: "10px" , marginLeft: "150px"}}><b>See what’s happening in <br></br> the world right now</b></TextTitle>
+                    <TextTitle title style={{ fontSize: "16px", marginBottom: "15px" , marginLeft: "150px" }}><b>Join Twitter today.</b></TextTitle>
                     
                     {/* <ThemeButton full size="large" primary type="button" onClick={setAuth}></ThemeButton> */}
-                    <ThemeButton full size="large" primary type="button" style={{ marginBottom: "15px" , marginLeft: "200px", width:"50%" }}>
+                    <ThemeButton full size="large" primary type="button" style={{ marginBottom: "15px" , marginLeft: "150px", width:"60%" }}>
                     Sign up
                    </ThemeButton>
 
-                   <ThemeButton full size="large" type="submit" style={{ marginBottom: "15px" , marginLeft: "200px", width:"50%" }}>
+                   <ThemeButton full size="large" type="submit" style={{ marginBottom: "15px" , marginLeft: "150px", width:"60%" }}>
                     Log in
                    </ThemeButton>
                </div>
